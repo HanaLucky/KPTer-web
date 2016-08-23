c8a46d965a1de66c3dfb1e5dcaa8510dcc419f42
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2016-08-23 23:10:30 +0900

    add describe to user rspec

--- kpter/spec/models/user_spec.rb
+++ kpter/spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Userに紐付いたCommunityをIDの降順を取得します。" do
+
+    context "正常系" do
+
+      it "Userが取得できること" do
+      end
+
+      it "Communityが取得できること" do
+      end
+
+      it "IDの降順で取得できること" do
+      end
+
+    end
+
+  end
+
 end
