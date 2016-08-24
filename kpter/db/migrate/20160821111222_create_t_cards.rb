cfa4b41af0d89d733f9d0a73dd587fbfc85e7b58
luckyriver0 <lucky.river82@gmail.com>
2016-08-24 22:19:12 +0900

    has_manyの追加＆期限カラム追加

--- kpter/db/migrate/20160821111222_create_t_cards.rb
+++ kpter/db/migrate/20160821111222_create_t_cards.rb
@@ -5,6 +5,7 @@ class CreateTCards < ActiveRecord::Migration[5.0]
       t.string :title, comment: "タイトル"
       t.string :detail, limit: 512, comment: "詳細"
       t.references :user, foreing_key: true, comment: "担当者"
+      t.date :deadline_on, comment: "期限"
       t.string :status, limit: 16, null: false, default: "open", comment: "ステータス¥topen, done"
       t.integer :x, null: false, default: 0, comment: "X座標"
       t.integer :y, null: false, default: 0, comment: "Y座標"
