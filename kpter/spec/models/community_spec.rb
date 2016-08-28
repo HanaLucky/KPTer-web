69bd716a1a2c74147dda919cf86b8710aba71dc7
luckyriver0 <lucky.river82@gmail.com>
2016-08-28 17:43:01 +0900

    条件が網羅されるように修正

--- kpter/spec/models/community_spec.rb
+++ kpter/spec/models/community_spec.rb
@@ -11,35 +11,88 @@ RSpec.describe Community, type: :model do
   end
 
   describe "コミュニティに関連する情報を取得する" do
-    it "指定したコミュニティIDに紐づくコミュニティが取得できること" do
+    context "コミュニティ、ボード、Tカードが存在する場合" do
+      it "指定したコミュニティIDに紐づくコミュニティが取得できること" do
+      end
+      it "コミュニティに紐づくボードが取得できること" do
+      end
+      it "ボードに紐づくt_cardsが取得できること" do
+      end
     end
-    it "コミュニティに紐づくボードが取得できること" do
+    context "コミュニティ、ボードが存在し、Tカードが存在しない場合" do
+      it "指定したコミュニティIDに紐づくコミュニティが取得できること" do
+      end
+      it "コミュニティに紐づくボードが取得できること" do
+      end
+      it "ボードに紐づくt_cardsがnilであること" do
+      end
     end
-    it "ボードに紐づくt_cardsが取得できること" do
+    context "コミュニティが存在し、ボード、Tカードが存在しない場合" do
+      it "指定したコミュニティIDに紐づくコミュニティが取得できること" do
+      end
+      it "コミュニティに紐づくボードがnilであること" do
+      end
+    end
+    context "コミュニティ、ボード、Tカードが存在しない場合" do
+      it "コミュニティがnilであること" do
+      end
     end
   end
 
   describe "コミュニティに紐づくt_cardsを取得する" do
-    it "t_cardsの数がinputで入れたデータの数に一致すること" do
+    context "Tカードが１つ以上存在する場合" do
+      it "t_cardsの数がinputで入れたデータの数に一致すること" do
+      end
+    end
+    context "Tカードが存在しない場合" do
+      it "nilが返却されること" do
+      end
+    end
+  end
+
+
+  describe "コミュニティ内に既に参加済みかどうかチェックする" do
+    context "参加済みの場合" do
+      it "trueが返却されること" do
+      end
+    end
+    context "参加していない場合" do
+      it "falseが返却されること" do
+      end
     end
   end
 
   describe "コミュニティ内にユーザーを参加させる" do
-    it "コミュニティ・ユーザー関連テーブルに登録したコミュニティIDが引数に指定したIDと一致すること" do
+    context "参加していない場合" do
+      it "コミュニティ・ユーザー関連テーブルに登録したコミュニティIDが引数に指定したIDと一致すること" do
+      end
+      it "コミュニティ・ユーザー関連テーブルに登録したユーザーIDが引数に指定したIDと一致すること" do
+      end
     end
-    it "コミュニティ・ユーザー関連テーブルに登録したユーザーIDが引数に指定したIDと一致すること" do
+    context "既に参加している場合" do
+      it "エラーメッセージが設定されること" do
+      end
+      it "コミュニティ・ユーザー関連テーブルに登録したユーザーIDが存在しないこと" do
+      end
     end
   end
 
   describe "コミュニティ内からユーザーを除名する" do
     it "コミュニティID, ユーザーIDに紐づくコミュニティ・ユーザー関連テーブルが削除されること" do
     end
+    # 既に除名済みかどうかのチェックは不要
   end
 
   describe "コミュニティ内のユーザーを全員取得する" do
-    it "コミュニティIDに紐づくコミュニティ・ユーザー関連テーブルが取得できること" do
+    context "コミュニティ内にユーザーが１人以上いる場合" do
+      it "コミュニティIDに紐づくコミュニティ・ユーザー関連テーブルが取得できること" do
+      end
+      it "コミュニティ・ユーザー関連テーブルのユーザーIDに紐づくユーザーが取得できること" do
+      end
     end
-    it "コミュニティ・ユーザー関連テーブルのユーザーIDに紐づくユーザーが取得できること" do
+    context "コミュニティ内にユーザーが存在しない場合" do
+      it "nilが返却されること" do
+      end
     end
   end
 end
