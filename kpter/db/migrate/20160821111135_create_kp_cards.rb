661f731c71253924bda664da59cee9a95e781793
luckyriver0 <lucky.river82@gmail.com>
2016-09-13 18:14:13 +0900

    enumerizeの導入

--- kpter/db/migrate/20160821111135_create_kp_cards.rb
+++ kpter/db/migrate/20160821111135_create_kp_cards.rb
@@ -4,7 +4,7 @@ class CreateKpCards < ActiveRecord::Migration[5.0]
       t.references :board, foreign_key: true, null: false, comment: "ボードID"
       t.string :title, comment: "タイトル"
       t.string :detail, limit: 512, comment: "詳細"
-      t.string :card_type, limit: 16, null: false, comment: "カード種別¥tkeep, problem"
+      t.string :card_type, limit: 16, null: false, comment: "カード種別"
       t.integer :x, null: false, default: 0, comment: "X座標"
       t.integer :y, null: false, default: 0, comment: "Y座標"
       t.integer :order, default: 0, comment: "重ね順"
