6b0315bb6b136357d7ac22cb3b71d8569f47e39e
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2016-09-15 12:44:02 +0900

    implemented user model

--- kpter/app/models/user.rb
+++ kpter/app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :omniauthable
   has_many :community_users
+  class << self
+    def find_communities_with_user_id(user_id)
+      Array(User.includes([{ :community_users => :community }])
+        .references(:community_users).order("communities.id DESC").find(user_id)
+        ).map { |u| u.community_users.map { |cu| cu.community }}.flatten
+    end
+
+    def find_tcards_with_user_id(user_id: user_id, status: "Open")
+      Array(User.includes([:community_users => [ :community => [ :boards => [ :t_cards ]]]])
+        .references(:community_users)
+        .order("communities.id DESC").where("t_cards.status = ?", status)
+        .find(user_id)
+        ).map { |u| u.community_users.map { |cu| cu.community.boards.map { |b| b.t_cards } }}
+        .flatten
+    end
+  end
+
 end
--- kpter/spec/models/user_spec.rb
+++ kpter/spec/models/user_spec.rb
@@ -7,8 +7,23 @@ RSpec.describe User, type: :model do
     context "正常系" do
 
       before do
-        @user = User.find(1)
-        @communities = @user.find_communities
+        @user = FactoryGirl.create(:user)
+        @community = Community.create(
+          name: "test_community"
+        )
+        @community2 = Community.create(
+          name: "test_community2"
+        )
+        @cu = CommunityUser.create(
+          user_id: @user.id,
+          community_id: @community.id
+        )
+        @cu2 = CommunityUser.create(
+          user_id: @user.id,
+          community_id: @community2.id
+        )
+
+        @communities = User.find_communities_with_user_id(@user.id)
       end
 
       it "Userに紐付いたCommunityが取得できること" do
@@ -16,7 +31,7 @@ RSpec.describe User, type: :model do
       end
 
       it "CommunityのIDの降順で取得できること" do
-        expect(@communities).to be_desc = -> (community) { community.id }
+        expect(@communities).to be_desc -> (community) { community.id }
       end
 
     end
@@ -25,25 +40,75 @@ RSpec.describe User, type: :model do
 
   describe "Userに紐付いたTCardを引数があれば、statusで絞込、期限でソートして取得します。" do
 
+    before do
+      @user = FactoryGirl.create(:user)
+      @community = Community.create(
+        name: "test_community"
+      )
+      @community2 = Community.create(
+        name: "test_community2"
+      )
+      @cu = CommunityUser.create(
+        user_id: @user.id,
+        community_id: @community.id
+      )
+      @cu2 = CommunityUser.create(
+        user_id: @user.id,
+        community_id: @community2.id
+      )
+
+      @board = Board.create(
+        community_id: @community.id,
+        name: "test_board_name"
+      )
+
+      @t_card_open = TCard.create(
+        board_id: @board.id,
+        title: "test_try_card1",
+        detail: "default status (open)",
+        status: TCard.status.open
+      )
+
+      @t_card_open2 = TCard.create(
+        board_id: @board.id,
+        title: "test_try_card2",
+        detail: "default status (open)",
+        status: TCard.status.open
+      )
+
+      @t_card_close = TCard.create(
+        board_id: @board.id,
+        title: "test_try_card3",
+        detail: "closed status",
+        status: TCard.status.closed
+      )
+      @t_card_close2 = TCard.create(
+        board_id: @board.id,
+        title: "test_try_card4",
+        detail: "closed status",
+        status: TCard.status.closed
+      )
+
+    end
+
     context "引数なし" do
 
       before do
-        @user = User.find(1)
-        @t_cards = @user.find_tcards
+        @open_tcards = User.find_tcards_with_user_id(user_id: @user.id, status: TCard.status.open)
       end
 
       it "Userに紐づくTCardが取得できること" do
-        expect(@t_cards).to be_present
+        expect(@open_tcards).to be_present
       end
 
       it "OpenなstatusのTCardが取得できること" do
-        expect(@t_cards).to be_all -> t_card {
-          t_card == "Open"
+        expect(@open_tcards).to be_all -> t_card {
+          t_card.status == TCard.status.open
         }
       end
 
       it "期限日昇順でTCardが取得できること" do
-        expect(@t_cards).to be_asc = -> (t_card) { t_card.deadline }
+        expect(@open_tcards).to be_asc -> (t_card) { t_card.deadline }
       end
 
     end
@@ -51,22 +116,21 @@ RSpec.describe User, type: :model do
     context "引数両方" do
 
       before do
-        @user = User.find(1)
-        @t_cards = @user.find_tcards
+        @closed_tcards = User.find_tcards_with_user_id(user_id: @user.id, status: TCard.status.closed)
       end
 
       it "Userに紐づくTCardが取得できること" do
-        expect(@t_cards).to be_present
+        expect(@closed_tcards).to be_present
       end
 
       it "CloseなstatusのTCardが取得できること" do
-        expect(@t_cards).to be_all -> t_card {
-          t_card == "Closed"
+        expect(@closed_tcards).to be_all -> t_card {
+          t_card.status == TCard.status.closed
         }
       end
 
       it "期限日昇順でTCardが取得できること" do
-        expect(@t_cards).to be_asc = -> (t_card) { t_card.deadline }
+        expect(@closed_tcards).to be_asc -> (t_card) { t_card.deadline }
       end
 
     end
