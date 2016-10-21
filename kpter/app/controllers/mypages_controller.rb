570a879fd6627a27ad7b4beecc909dc6ebfacf39
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2016-10-21 13:36:22 +0900

    サイドカラム実装

--- kpter/app/controllers/mypages_controller.rb
+++ kpter/app/controllers/mypages_controller.rb
@@ -3,6 +3,7 @@ class MypagesController < ApplicationController
     @communities = User.find_communities_with_user_id(current_user.id)
     @all_tcards = User.find_tcards_with_user_id(current_user.id)
     @t_cards = Kaminari.paginate_array(@all_tcards).page(params[:page])
+    @community = Community.new
   end
 
   def toggle
@@ -11,8 +12,29 @@ class MypagesController < ApplicationController
   end
 
   def refresh_tasks
-    @all_tcards = User.find_tcards_with_user_id(current_user.id, params[:status])
+    status = params[:status]
+    status ||= TCard.status.open
+    @all_tcards = User.find_tcards_with_user_id(current_user.id, status)
     @t_cards = Kaminari.paginate_array(@all_tcards).page(params[:page])
   end
 
+  def create_community
+    @community = Community.new(name: params[:community][:name])
+
+    respond_to do |format|
+      ActiveRecord::Base.transaction do
+        if @community.save
+          @community_user = CommunityUser.new(user_id: current_user.id, community_id: @community.id)
+          if @community_user.save
+            format.html { redirect_to @community, notice: 'Community was successfully created.' }
+            format.json { render :show, status: :created, location: @community }
+          else
+            format.html { render :show }
+          end
+        else
+          format.html { render :show }
+        end
+      end
+    end
+  end
 end
