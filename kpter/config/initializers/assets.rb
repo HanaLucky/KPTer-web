f01d432025574ac193a04243943f308d73ddd62c
luckyriver0 <lucky.river82@gmail.com>
2016-10-22 14:45:25 +0900

    タスク一覧実装

--- kpter/config/initializers/assets.rb
+++ kpter/config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( communities/show.js )
+Rails.application.config.assets.precompile += %w( dataTablesSetting.js )
+Rails.application.config.assets.precompile += %w( tasksTable.js )
