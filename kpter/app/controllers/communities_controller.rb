fd283546e446de02f6c8136e8fd7572766a67a23
luckyriver0 <lucky.river82@gmail.com>
2016-10-22 14:44:53 +0900

    タスク一覧実装

--- kpter/app/controllers/communities_controller.rb
+++ kpter/app/controllers/communities_controller.rb
@@ -5,7 +5,21 @@ class CommunitiesController < ApplicationController
     @community = Community.find(params[:id])
     @all_boards = @community.find_boards
     @boards = Kaminari.paginate_array(@all_boards).page(params[:page]).per(5)
-    @tasks = @community.find_tcards
+    @all_tcards = @community.find_tcards
+    @t_cards = Kaminari.paginate_array(@all_tcards).page(params[:page]).per(10)
     @attendees = @community.find_users
+  end
+
+  def toggle
+      render nothing: true
+      TCard.update_status(params[:t_card_id])
+  end
+
+  def refresh_tasks
+    status = params[:status]
+    status ||= TCard.status.open
+    @community = Community.find(params[:id])
+    @all_tcards = @community.find_tcards(status)
+    @t_cards = Kaminari.paginate_array(@all_tcards).page(params[:page])
   end
 end
