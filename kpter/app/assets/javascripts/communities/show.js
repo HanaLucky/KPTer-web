80f3dbbf3f1e8402cd41b472fde12ab73ca98f1e
luckyriver0 <lucky.river82@gmail.com>
2016-11-12 23:45:01 +0900

    コミュニティ名も文字数制限を32に変更

--- kpter/app/assets/javascripts/communities/show.js
+++ kpter/app/assets/javascripts/communities/show.js
@@ -1,7 +1,7 @@
 $(function() {
   // read more. https://vitalets.github.io/x-editable/index.html
   $('#community-name').editable({
-    tpl: '<input type="text" maxlength=33 style="width:450px;">',
+    tpl: '<input type="text" maxlength=32 style="width:450px;">',
     success: function(response, newValue) {
         // 左サイドナビのコミュニティ名を書き換える
         $('#navi-communities li .active').html('<i class="fa fa-comment fa-fw"></i>' + newValue);
