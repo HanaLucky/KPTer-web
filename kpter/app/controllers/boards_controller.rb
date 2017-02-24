176b2e2eb0a292f72746cd237ebbe471dead2894
luckyriver0 <lucky.river82@gmail.com>
2017-02-24 15:19:56 +0900

    ログインチェックフィルター追加

--- kpter/app/controllers/boards_controller.rb
+++ kpter/app/controllers/boards_controller.rb
@@ -1,4 +1,7 @@
 class BoardsController < ApplicationController
+  before_filter :store_location
+  before_filter :authenticate_user!
+  
   def create
     @board = Board.create(name: params[:board][:name], community_id: params[:community_id])
     redirect_to controller: 'boards', action: 'show', id: @board.id
