c1b648bdd8b8d3d03600099ae3d72328adfc8d81
luckyriver0 <lucky.river82@gmail.com>
2017-03-14 18:50:39 +0900

    デバッグプリント除去

--- kpter/app/controllers/confirm_controller.rb
+++ kpter/app/controllers/confirm_controller.rb
@@ -2,7 +2,6 @@ class ConfirmController < ApplicationController
   before_action :store_location
 
   def show
-    puts "================="
     # sign_up画面以外からきた場合は、sign_up画面へ飛ばす
     # Users::RegistrationsController->after_inactive_sign_up_path_for参照
     unless session[:route_from_inactive_sign_up]
