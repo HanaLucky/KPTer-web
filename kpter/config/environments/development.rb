dff7faa3a989fb31a64a8d946b12462b9c5d2f31
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2017-04-22 14:34:12 +0900

    fix bug

--- kpter/config/environments/development.rb
+++ kpter/config/environments/development.rb
@@ -1,4 +1,3 @@
-require "socket"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -40,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { host: 192.168.33.60, port: 3000 }
+  config.action_mailer.default_url_options = { host: "192.168.33.60", port: 3000 }
 
   # whitelist
   config.web_console.whitelisted_ips = '0.0.0.0/0'
