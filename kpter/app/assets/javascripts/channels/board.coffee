cee6440b1f419deab388552788d8404f51235686
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2017-06-11 16:40:12 +0900

    カード作成

--- kpter/app/assets/javascripts/channels/board.coffee
+++ kpter/app/assets/javascripts/channels/board.coffee
@@ -6,8 +6,16 @@ App.board = App.cable.subscriptions.create "BoardChannel",
     # Called when the subscription has been terminated by the server
 
   received: (data) ->
-    alert data['x']
-    $('#boardWrap').append data['message']
+    if data['kpcard']
+      $('#boardWrap').append data['kpcard']
 
-  create_tcard: (board_id, title, x, y) ->
-    @perform 'create_tcard', board_id: board_id, title: title, x: x, y: y
+    if data['tcard']
+      $('#boardWrap').append data['tcard']
+
+    $('.cardBox').draggable();
+
+  create_kpcard: (card_type, title, board_id, x, y) ->
+    @perform 'create_kpcard', card_type: card_type, title: title, board_id: board_id, x: x, y: y
+
+  create_tcard: (title, board_id, x, y) ->
+    @perform 'create_tcard', title: title, board_id: board_id, x: x, y: y
--- kpter/app/channels/board_channel.rb
+++ kpter/app/channels/board_channel.rb
@@ -7,11 +7,25 @@ class BoardChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def create_tcard(data)
-    p "******title*******"
-    p data['title']
-    p "***board-id***"
-    p data['board_id']
-    TCard.create! title: data['title'], board_id: data['board_id'], x: data['x'], y: data['y']
+  def create_kpcard(data)
+    kpcard = KpCard.create! card_type: data['card_type'], title: data['title'], board_id: data['board_id'], x: data['x'], y: data['y']
+    ActionCable.server.broadcast 'board_channel', kpcard: render_kpcard(kpcard)
+
   end
+
+  def create_tcard(data)
+    tcard = TCard.create! title: data['title'], user_id: current_user.id, board_id: data['board_id'], x: data['x'], y: data['y']
+    ActionCable.server.broadcast 'board_channel', tcard: render_tcard(tcard)
+  end
+
+  private
+
+  def render_kpcard(kp_card)
+    ApplicationController.renderer.render(partial: 'cards/kpcard', locals: { kpcard: kp_card })
+  end
+
+  def render_tcard(t_card)
+    ApplicationController.renderer.render(partial: 'cards/tcard', locals: { tcard: t_card })
+  end
+
 end
