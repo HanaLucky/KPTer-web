105a7d4ef4b637ad2b75d19c11c336f27d323023
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2017-06-18 16:39:00 +0900

    asynchronous broadcast

--- kpter/app/jobs/card_broadcast_job.rb
+++ kpter/app/jobs/card_broadcast_job.rb
@@ -1,12 +1,21 @@
 class CardBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(t_card)
-    ActionCable.server.broadcast 'board_channel', tcard: render_tcard(t_card)
+  def perform(card)
+    if card.class == KpCard
+      ActionCable.server.broadcast 'board_channel', kpcard: render_kpcard(card)
+    elsif card.class == TCard
+      ActionCable.server.broadcast 'board_channel', tcard: render_tcard(card)
+    end
   end
 
   private
-    def render_tcard(t_card)
-      ApplicationController.renderer.render(partial: 'cards/tcard', locals: { tcard: t_card })
-    end
+
+  def render_kpcard(card)
+    ApplicationController.renderer.render(partial: 'cards/kpcard', locals: { kpcard: card })
+  end
+
+  def render_tcard(card)
+    ApplicationController.renderer.render(partial: 'cards/tcard', locals: { tcard: card })
+  end
 end
--- kpter/app/models/kp_card.rb
+++ kpter/app/models/kp_card.rb
@@ -2,4 +2,6 @@ class KpCard < ApplicationRecord
   extend Enumerize
   enumerize :card_type, in: [:keep, :problem]
   belongs_to :board
+
+  after_create_commit { CardBroadcastJob.perform_later self }
 end
