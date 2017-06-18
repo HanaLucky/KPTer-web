b4e12044f77a19268971b71967260803e631836a
luckyriver0 <lucky.river82@gmail.com>
2017-06-18 15:12:17 +0900

    neglect-of-test

--- kpter/app/helpers/application_helper.rb
+++ kpter/app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 
   # avatar image url
   def avatar_url(user)
-    unless user
-      if user.avatar.file.nil?
+    if user
+      unless user.avatar.file
         url = asset_path("noimages/profile_#{user.id % 3}.png")
       else
         url = "#{asset_path(user.avatar)}?#{user.lock_version}"
