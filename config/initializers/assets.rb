24bb20a6c5eff3e4b699c01973dc830bacaff036
luckyriver0 <lucky.river82@gmail.com>
2018-03-04 16:08:46 +0900

    リファクタリング

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( mdl/1.3.0/material.kpter-pink.min.css )
-Rails.application.config.assets.precompile += %w( mdl/mdl-selectfield.js )
+Rails.application.config.assets.precompile += %w( mdl/1.3.0/material.js )
 
 Rails.application.config.assets.precompile += %w( style.css )
 
