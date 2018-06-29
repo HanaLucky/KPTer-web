4830fbae47ffe285de2c5ab86b81b6f18362b3a1
luckyriver0 <lucky.river82@gmail.com>
2018-06-29 23:00:42 +0900

    メールのローカライズ対応

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,6 +20,11 @@ Devise.setup do |config|
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
 
+  # to configure mailer layout
+  Rails.application.config.to_prepare do
+    Devise::Mailer.layout "email" # email.haml or email.erb
+  end
+
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
